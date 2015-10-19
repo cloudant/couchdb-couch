0f68d04eddc3f62ec5c370a575cd601ea82e8089
Paul J. Davis <paul.joseph.davis@gmail.com>
2015-10-19 16:04:36 -0500

    WIP: First pass at purge

--- test/engines/test_engine_util.erl
+++ test/engines/test_engine_util.erl
@@ -65,21 +65,26 @@ apply_action(Engine, St, Action) ->
 
 apply_batch(Engine, St, Actions) ->
     UpdateSeq = Engine:get(St, update_seq) + 1,
-    AccIn = {UpdateSeq, [], []},
+    AccIn = {UpdateSeq, [], [], []},
     AccOut = lists:foldl(fun(Action, Acc) ->
-        {SeqAcc, DocAcc, LDocAcc} = Acc,
+        {SeqAcc, DocAcc, LDocAcc, PurgeAcc} = Acc,
         case Action of
             {_, {<<"_local/", _/binary>>, _}} ->
                 LDoc = gen_local_write(Engine, St, Action),
-                {SeqAcc, DocAcc, [LDoc | LDocAcc]};
+                {SeqAcc, DocAcc, [LDoc | LDocAcc], PurgeAcc};
             _ ->
-                Doc = gen_write(Engine, St, Action, SeqAcc),
-                {SeqAcc + 1, [Doc | DocAcc], LDocAcc}
+                case gen_write(Engine, St, Action, SeqAcc) of
+                    {_OldFDI, _NewFDI} = Pair ->
+                        {SeqAcc + 1, [Pair | DocAcc], LDocAcc, PurgeAcc};
+                    {Pair, NewSeqAcc, NewPurgeInfo} ->
+                        NewPurgeAcc = [NewPurgeInfo | PurgeAcc],
+                        {NewSeqAcc, [Pair | DocAcc], LDocAcc, NewPurgeAcc}
+                end
         end
     end, AccIn, Actions),
-    {_, Docs0, LDocs} = AccOut,
+    {_, Docs0, LDocs, PurgeIdRevs} = AccOut,
     Docs = lists:reverse(Docs0),
-    {ok, NewSt} = Engine:write_doc_infos(St, Docs, LDocs, []),
+    {ok, NewSt} = Engine:write_doc_infos(St, Docs, LDocs, PurgeIdRevs),
     NewSt.
 
 
@@ -104,7 +109,6 @@ gen_local_write(Engine, St, {Action, {DocId, Body}}) ->
         body = Body,
         deleted = Deleted
     }.
-
 
 gen_write(Engine, St, {Action, {DocId, Body}}, UpdateSeq) ->
     gen_write(Engine, St, {Action, {DocId, Body, []}}, UpdateSeq);
@@ -138,6 +142,42 @@ gen_write(Engine, St, {create, {DocId, Body, Atts0}}, UpdateSeq) ->
         sizes = Sizes
     }};
 
+gen_write(Engine, St, {purge, {DocId, PrevRevs, _}}, UpdateSeq) ->
+    [#full_doc_info{} = PrevFDI] = Engine:open_docs(St, [DocId]),
+
+    #full_doc_info{
+        rev_tree = PrevTree
+    } = PrevFDI,
+
+    {NewTree, RemRevs} = couch_key_tree:remove_leafs(PrevTree, PrevRevs),
+    RemovedAll = lists:sort(RemRevs) == lists:sort(PrevRevs),
+    if RemovedAll -> ok; true ->
+        % If we didn't purge all the requested revisions
+        % then its a bug in the test.
+        erlang:error({invalid_purge_test_revs, PrevRevs})
+    end,
+
+    case NewTree of
+        [] ->
+            % We've completely purged the document
+            {{PrevFDI, not_found}, UpdateSeq + 1, {DocId, RemRevs}};
+        _ ->
+            % We have to relabel the update_seq of all
+            % leaves. See couch_db_updater for details.
+            {NewNewTree, NewUpdateSeq} = couch_key_tree:mapfold(fun
+                (_RevId, Leaf, leaf, InnerSeqAcc) ->
+                    {Leaf#leaf{seq = InnerSeqAcc + 1}, InnerSeqAcc + 1};
+                (_RevId, Value, _Type, InnerSeqAcc) ->
+                    {Value, InnerSeqAcc}
+            end, UpdateSeq, NewTree),
+            NewFDI = PrevFDI#full_doc_info{
+                update_seq = NewUpdateSeq,
+                rev_tree = NewNewTree
+            },
+            {{PrevFDI, NewFDI}, NewUpdateSeq + 1, {DocId, RemRevs}}
+    end;
+    
+
 gen_write(Engine, St, {Action, {DocId, Body, Atts0}}, UpdateSeq) ->
     [#full_doc_info{} = PrevFDI] = Engine:open_docs(St, [DocId]),
     Atts = [couch_att:to_disk_term(Att) || Att <- Atts0],
@@ -151,12 +191,13 @@ gen_write(Engine, St, {Action, {DocId, Body, Atts0}}, UpdateSeq) ->
         rev = PrevRev
     } = prev_rev(PrevFDI),
 
-    Rev = crypto:hash(md5, term_to_binary({DocId, PrevRev, Body, Atts})),
+    Rev = gen_revision(Action, DocId, PrevRev, Body, Atts),
     Summary = make_doc_summary(Engine, St, {Body, Atts}),
     {ok, Ptr, Len} = Engine:write_doc_summary(St, Summary),
 
     Deleted = case Action of
         update -> false;
+        conflict -> false;
         delete -> true
     end,
 
@@ -174,18 +215,34 @@ gen_write(Engine, St, {Action, {DocId, Body, Atts0}}, UpdateSeq) ->
     },
 
     {RevPos, PrevRevId} = PrevRev,
-    Tree = {RevPos, {PrevRevId, ?REV_MISSING, [{Rev, Leaf, []}]}},
+    Tree = gen_tree(Action, RevPos, PrevRevId, Rev, Leaf),
     RevsLimit = Engine:get(St, revs_limit),
     {NewTree, new_leaf} = couch_key_tree:merge(PrevRevTree, Tree, RevsLimit),
 
     NewFDI = PrevFDI#full_doc_info{
-        deleted = Deleted,
+        deleted = couch_doc:is_deleted(NewTree),
         update_seq = UpdateSeq,
         rev_tree = NewTree,
         sizes = Sizes
     },
 
     {PrevFDI, NewFDI}.
+
+
+gen_revision(conflict, DocId, _PrevRev, Body, Atts) ->
+    crypto:hash(md5, term_to_binary({DocId, Body, Atts}));
+gen_revision(delete, DocId, PrevRev, Body, Atts) ->
+    gen_revision(update, DocId, PrevRev, Body, Atts);
+gen_revision(update, DocId, PrevRev, Body, Atts) ->
+    crypto:hash(md5, term_to_binary({DocId, PrevRev, Body, Atts})).
+
+
+gen_tree(conflict, _RevPos, _PrevRevId, Rev, Leaf) ->
+    [{0, {Rev, Leaf, []}}];
+gen_tree(delete, RevPos, PrevRevId, Rev, Leaf) ->
+    gen_tree(update, RevPos, PrevRevId, Rev, Leaf);
+gen_tree(update, RevPos, PrevRevId, Rev, Leaf) ->
+    {RevPos, {PrevRevId, ?REV_MISSING, [{Rev, Leaf, []}]}}.
 
 
 make_doc_summary(Engine, St, DocData) ->
