c354ccfcd9bffbf735798487414c8d5a2379f512
Paul J. Davis <paul.joseph.davis@gmail.com>
2016-02-05 12:21:39 -0600

    Add storage engine test suite
    This allows other storage engine implementations to reuse the same exact
    test suite without having to resort to shenanigans like keeping vendored
    copies up to date.

---
+++ src/test_engine_get_set_props.erl
@@ -0,0 +1,94 @@
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+-module(test_engine_get_set_props).
+-compile(export_all).
+
+
+-include_lib("eunit/include/eunit.hrl").
+
+
+cet_default_props() ->
+    Engine = test_engine_util:get_engine(),
+    DbPath = test_engine_util:dbpath(),
+
+    {ok, St} = Engine:init(DbPath, [
+            create,
+            {default_security_object, dso}
+        ]),
+
+    Node = node(),
+
+    ?assertEqual(0, Engine:get(St, doc_count)),
+    ?assertEqual(0, Engine:get(St, del_doc_count)),
+    ?assertEqual(true, is_list(Engine:get(St, size_info))),
+    ?assertEqual(true, is_integer(Engine:get(St, disk_version))),
+    ?assertEqual(0, Engine:get(St, update_seq)),
+    ?assertEqual(0, Engine:get(St, purge_seq)),
+    ?assertEqual([], Engine:get(St, last_purged)),
+    ?assertEqual(dso, Engine:get(St, security)),
+    ?assertEqual(1000, Engine:get(St, revs_limit)),
+    ?assertMatch(<<_:32/binary>>, Engine:get(St, uuid)),
+    ?assertEqual([{Node, 0}], Engine:get(St, epochs)),
+    ?assertEqual(0, Engine:get(St, compacted_seq)).
+
+
+cet_set_security() ->
+    check_prop_set(security, dso, [{<<"readers">>, []}]).
+
+
+cet_set_revs_limit() ->
+    check_prop_set(revs_limit, 1000, 50).
+
+
+cet_set_epochs() ->
+    TestValue = [
+        {'other_node@127.0.0.1', 0},
+        {node(), 0}
+    ],
+    % This looks weird. I need to move the epochs
+    % logic to a central place so this isn't the case here.
+    % The weirdness is because epochs are modified when
+    % we use couch_bt_engine_header.
+    CommittedValue = [
+        {node(), 1},
+        {'other_node@127.0.0.1', 0}
+    ],
+    check_prop_set(epochs, [{node(), 0}], TestValue, CommittedValue).
+
+
+cet_set_compact_seq() ->
+    check_prop_set(compacted_seq, 0, 12).
+
+
+check_prop_set(Name, Default, Value) ->
+    check_prop_set(Name, Default, Value, Value).
+
+
+check_prop_set(Name, Default, Value, CommittedValue) ->
+    Engine = test_engine_util:get_engine(),
+    DbPath = test_engine_util:dbpath(),
+
+    {ok, St0} = Engine:init(DbPath, [
+            create,
+            {default_security_object, dso}
+        ]),
+    ?assertEqual(Default, Engine:get(St0, Name)),
+
+    {ok, St1} = Engine:set(St0, Name, Value),
+    ?assertEqual(Value, Engine:get(St1, Name)),
+
+    {ok, St2} = Engine:commit_data(St1),
+    Engine:terminate(normal, St2),
+
+    {ok, St3} = Engine:init(DbPath, []),
+    ?assertEqual(CommittedValue, Engine:get(St3, Name)).
---
+++ src/test_engine_purge_docs.erl
@@ -0,0 +1,158 @@
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+-module(test_engine_purge_docs).
+-compile(export_all).
+
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("couch/include/couch_db.hrl").
+
+
+cet_purge_simple() ->
+    {ok, Engine, St1} = test_engine_util:init_engine(),
+
+    Actions1 = [
+        {create, {<<"foo">>, [{<<"vsn">>, 1}]}}
+    ],
+    {ok, St2} = test_engine_util:apply_actions(Engine, St1, Actions1),
+
+    ?assertEqual(1, Engine:get(St2, doc_count)),
+    ?assertEqual(0, Engine:get(St2, del_doc_count)),
+    ?assertEqual(1, Engine:get(St2, update_seq)),
+    ?assertEqual(0, Engine:get(St2, purge_seq)),
+    ?assertEqual([], Engine:get(St2, last_purged)),
+
+    [FDI] = Engine:open_docs(St2, [<<"foo">>]),
+    PrevRev = test_engine_util:prev_rev(FDI),
+    Rev = PrevRev#rev_info.rev,
+
+    Actions2 = [
+        {purge, {<<"foo">>, Rev}}
+    ],
+    {ok, St3} = test_engine_util:apply_actions(Engine, St2, Actions2),
+
+    ?assertEqual(0, Engine:get(St3, doc_count)),
+    ?assertEqual(0, Engine:get(St3, del_doc_count)),
+    ?assertEqual(2, Engine:get(St3, update_seq)),
+    ?assertEqual(1, Engine:get(St3, purge_seq)),
+    ?assertEqual([{<<"foo">>, [Rev]}], Engine:get(St3, last_purged)).
+
+
+cet_purge_conflicts() ->
+    {ok, Engine, St1} = test_engine_util:init_engine(),
+
+    Actions1 = [
+        {create, {<<"foo">>, [{<<"vsn">>, 1}]}},
+        {conflict, {<<"foo">>, [{<<"vsn">>, 2}]}}
+    ],
+    {ok, St2} = test_engine_util:apply_actions(Engine, St1, Actions1),
+
+    ?assertEqual(1, Engine:get(St2, doc_count)),
+    ?assertEqual(0, Engine:get(St2, del_doc_count)),
+    ?assertEqual(2, Engine:get(St2, update_seq)),
+    ?assertEqual(0, Engine:get(St2, purge_seq)),
+    ?assertEqual([], Engine:get(St2, last_purged)),
+
+    [FDI1] = Engine:open_docs(St2, [<<"foo">>]),
+    PrevRev1 = test_engine_util:prev_rev(FDI1),
+    Rev1 = PrevRev1#rev_info.rev,
+
+    Actions2 = [
+        {purge, {<<"foo">>, Rev1}}
+    ],
+    {ok, St3} = test_engine_util:apply_actions(Engine, St2, Actions2),
+
+    ?assertEqual(1, Engine:get(St3, doc_count)),
+    ?assertEqual(0, Engine:get(St3, del_doc_count)),
+    ?assertEqual(4, Engine:get(St3, update_seq)),
+    ?assertEqual(1, Engine:get(St3, purge_seq)),
+    ?assertEqual([{<<"foo">>, [Rev1]}], Engine:get(St3, last_purged)),
+
+    [FDI2] = Engine:open_docs(St3, [<<"foo">>]),
+    PrevRev2 = test_engine_util:prev_rev(FDI2),
+    Rev2 = PrevRev2#rev_info.rev,
+
+    Actions3 = [
+        {purge, {<<"foo">>, Rev2}}
+    ],
+    {ok, St4} = test_engine_util:apply_actions(Engine, St3, Actions3),
+
+    ?assertEqual(0, Engine:get(St4, doc_count)),
+    ?assertEqual(0, Engine:get(St4, del_doc_count)),
+    ?assertEqual(5, Engine:get(St4, update_seq)),
+    ?assertEqual(2, Engine:get(St4, purge_seq)),
+    ?assertEqual([{<<"foo">>, [Rev2]}], Engine:get(St4, last_purged)).
+
+
+cet_add_delete_purge() ->
+    {ok, Engine, St1} = test_engine_util:init_engine(),
+
+    Actions1 = [
+        {create, {<<"foo">>, [{<<"vsn">>, 1}]}},
+        {delete, {<<"foo">>, [{<<"vsn">>, 2}]}}
+    ],
+
+    {ok, St2} = test_engine_util:apply_actions(Engine, St1, Actions1),
+
+    ?assertEqual(0, Engine:get(St2, doc_count)),
+    ?assertEqual(1, Engine:get(St2, del_doc_count)),
+    ?assertEqual(2, Engine:get(St2, update_seq)),
+    ?assertEqual(0, Engine:get(St2, purge_seq)),
+    ?assertEqual([], Engine:get(St2, last_purged)),
+
+    [FDI] = Engine:open_docs(St2, [<<"foo">>]),
+    PrevRev = test_engine_util:prev_rev(FDI),
+    Rev = PrevRev#rev_info.rev,
+
+    Actions2 = [
+        {purge, {<<"foo">>, Rev}}
+    ],
+    {ok, St3} = test_engine_util:apply_actions(Engine, St2, Actions2),
+
+    ?assertEqual(0, Engine:get(St3, doc_count)),
+    ?assertEqual(0, Engine:get(St3, del_doc_count)),
+    ?assertEqual(3, Engine:get(St3, update_seq)),
+    ?assertEqual(1, Engine:get(St3, purge_seq)),
+    ?assertEqual([{<<"foo">>, [Rev]}], Engine:get(St3, last_purged)).
+
+
+cet_add_two_purge_one() ->
+    {ok, Engine, St1} = test_engine_util:init_engine(),
+
+    Actions1 = [
+        {create, {<<"foo">>, [{<<"vsn">>, 1}]}},
+        {create, {<<"bar">>, []}}
+    ],
+
+    {ok, St2} = test_engine_util:apply_actions(Engine, St1, Actions1),
+
+    ?assertEqual(2, Engine:get(St2, doc_count)),
+    ?assertEqual(0, Engine:get(St2, del_doc_count)),
+    ?assertEqual(2, Engine:get(St2, update_seq)),
+    ?assertEqual(0, Engine:get(St2, purge_seq)),
+    ?assertEqual([], Engine:get(St2, last_purged)),
+
+    [FDI] = Engine:open_docs(St2, [<<"foo">>]),
+    PrevRev = test_engine_util:prev_rev(FDI),
+    Rev = PrevRev#rev_info.rev,
+
+    Actions2 = [
+        {purge, {<<"foo">>, Rev}}
+    ],
+    {ok, St3} = test_engine_util:apply_actions(Engine, St2, Actions2),
+
+    ?assertEqual(1, Engine:get(St3, doc_count)),
+    ?assertEqual(0, Engine:get(St3, del_doc_count)),
+    ?assertEqual(3, Engine:get(St3, update_seq)),
+    ?assertEqual(1, Engine:get(St3, purge_seq)),
+    ?assertEqual([{<<"foo">>, [Rev]}], Engine:get(St3, last_purged)).
