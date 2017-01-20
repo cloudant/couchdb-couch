f5a8f169607caeb00bcf1761df915f6b24edc1cd
Paul J. Davis <paul.joseph.davis@gmail.com>
2017-01-20 08:07:50 -0600

    Add couch_msgq_buffer
    This module exists due to some pecularities in the Erlang VM when a
    process gets a huge message queue. In a nutshell, a process will at
    certain points copy all messages onto its heap. If the message queue
    gets large enough and the process is busy enough, the process ends up
    spending most of its time in garbage collection.
    
    Putting a buffer in place that lets no more than N messages per second
    through will hopefully alleviate this issue by moving the large message
    queue to an intermediary pid that does little to no work per message
    which hopefully will delay garbage collection long enough that the
    message queue spike can be absorbed and recovered from.

---
+++ src/couch_msgq_buffer.erl
@@ -0,0 +1,132 @@
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+-module(couch_msgq_buffer).
+
+
+-export([
+    start_link/0
+]).
+
+-export([
+    init/3
+]).
+
+-export([
+    system_continue/3,
+    system_terminate/4,
+    system_get_state/1,
+    system_replace_state/2,
+    write_debug/3
+]).
+
+
+-define(DEFAULT_RATE, 10000).
+-define(WRITE_DEBUG, fun ?MODULE:write_debug/3).
+
+
+-record(st, {
+    name,
+    target,
+    second,
+    count,
+    rate
+}).
+
+
+start_link(Name, Target) ->
+    proc_lib:start_link(?MODULE, init, [self(), Name, Target]).
+
+
+init(Parent, Name, Target) ->
+    case register(Name, self()) of
+        true ->
+            proc_link:init_ack(Parent, {ok, self()}),
+            Debug = sys:debug_options([]),
+            {_, Second, _} = os:timestamp(),
+            St = #st{
+                name = Name,
+                target = Target,
+                second = Second,
+                count = 0,
+                rate = ?DEFAULT_RATE
+            },
+            loop(Parent, Debug, St)
+    catch error:_ ->
+        proc_lib:init_ack(Parent, {error, {already_started, where(Name)}})
+    end.
+
+
+loop(Parent, Debug, St) ->
+    #st{
+        name = Name,
+        target = Target,
+        second = Second,
+        count = Count,
+        rate = Rate
+    } = St,
+
+    {_, NewSecond, MicroSeconds} = os:timestamp(),
+
+    receive
+        {system, From, Request} ->
+            sys:handle_system_msg(Request, From, Parent, Name, Debug, St);
+
+        Msg ->
+            InLoc = {in, handle, From},
+            Debug2 = sys:handle_debug(Debug, ?WRITE_DEBUG, Name, InLoc),
+
+            NewCount = 1 + (if NewSecond == Second -> Count; true -> 0 end),
+
+            if NewCount < Rate -> ok; true ->
+                SleepMilli = 1000 - (MicroSeconds div 1000),
+                timer:sleep(SleepMilli)
+            end,
+
+            case whereis(Target) of
+                Pid when is_pid(Pid) ->
+                    Pid ! Msg;
+                undefined ->
+                    % Drop the message?
+                    ok
+            end,
+
+            OutLoc = {out, {handle, NewCount}, From},
+            Debug3 = sys:handle_debug(Debug2, ?WRITE_DEBUG, Name, OutLoc),
+
+            NewSt = St#st{
+                second = NewSecond,
+                count = NewCount
+            },
+            loop(Parent, Debug3, NewSt);
+    end.
+
+
+system_continue(Parent, Debug, St) ->
+    loop(Parent, Debug, St).
+
+
+system_terminate(Reason, _Parent, _Debug, _St) ->
+    exit(Reason).
+
+
+system_get_state(St) ->
+    {ok, St}.
+
+
+system_replace_state(StateFun, St) ->
+    NewSt = StateFun(St),
+    {ok, NewSt, NewSt}.
+
+
+write_debug(Dev, Event, Name) ->
+    io:format(Dev, "~p event = ~p~n", [Name, Event]).
